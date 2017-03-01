e5ff4238abd5583e6516035f1875926d69c393fd
Christian Borresen <christian@Christians-MacBook-Pro.local>
2017-03-01 10:11:01 +0400

    Introduces DefaultRouter(), shortening the code

--- stxclock/urls.py
+++ stxclock/urls.py
@@ -1,38 +1,17 @@
 from django.conf.urls import url, include
-from rest_framework import renderers
-from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
-from stxclock.views import ExchangeViewSet, UserViewSet, api_root
+from rest_framework.routers import DefaultRouter
 
 
-exchange_list = ExchangeViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-    })
-exchange_detail = ExchangeViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-    })
-user_list = UserViewSet.as_view({
-    'get': 'list'
-    })
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'
-    })
+# Create a router and register our viewset with it.
+router = DefaultRouter()
+router.register(r'exchanges', views.ExchangeViewSet)
+router.register(r'users', views.UserViewSet)
 
 
-# app_name = 'stxclock'
-urlpatterns = format_suffix_patterns([
-        url(r'^$', api_root),
-        url(r'^exchanges/$', exchange_list, name='exchange-list'),
-        url(r'^exchanges/(?P<pk>[0-9]+)/$', exchange_detail, name='exchange-detail'),
-        url(r'^users/$', user_list, name='user-list'),
-        url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail')
-        ])
-
-# Login and logout views for the browsable API
-urlpatterns += [
+# The API URLs are now determined automatically by the router.
+# Additionally, we include the login URLs for the browsable API.
+urlpatterns = [
+        url(r'^', include(router.urls)),
         url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
         ]
