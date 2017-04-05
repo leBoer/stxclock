24d858bbd4ab6d7250948ca9b36c2506c814503b
Nick Nisi <nick@nisi.org>
2017-04-05 08:49:14 +0400

    Removes the root url

--- stxclock/urls.py
+++ stxclock/urls.py
@@ -13,7 +13,7 @@ router.register(r'users', views.UserViewSet)
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browsable API.
 urlpatterns = [
-        url(r'^$', views.index, name='stxclock'),
+        # url(r'^$', views.index, name='stxclock'),
         url(r'^api/', include(router.urls)),
         url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
         ]
